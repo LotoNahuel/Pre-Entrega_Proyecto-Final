e44710eaaa3b51ce39dd6e3930aa12b3cc6773ed
NAHUEL LOTO <loto_nahuel@hotmail.com>
2023-03-27 17:46:46 -0300

    BotonesCorregidos_and_URL

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", inicioApp),
+    path("", inicioApp, name="inicio App"),
     path("crear_curso/", crear_curso),
-    path("cursos/", cursos),
-    path("profesores/", profesores),
-    path("estudiantes/", estudiantes),
-    path("entregables/", entregables),
+    path("cursos/", cursos, name="cursos"),
+    path("profesores/", profesores, name="profesores"),
+    path("estudiantes/", estudiantes, name="estudiantes"),
+    path("entregables/", entregables, name="entregables"),
 ]
